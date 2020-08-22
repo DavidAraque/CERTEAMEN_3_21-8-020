f4ae3cd87bf8ac59f1a7a40682d2ab79bb5f468e
DavidAraque <69062553+DavidAraque@users.noreply.github.com>
2020-08-21 20:08:55 -0400

    arreglo
    arreglo

---
+++ EJERCICIO_1/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Ingrese La Matriz NxN");
+            Console.Write("\nEntrada de un solo digito\n");
+            Console.Write("El tamaño de la matiz es de: ");
+            n = Convert.ToInt32(Console.ReadLine()); ;
+
+
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("\nmat[" + (i + 1) + "][" + (j + 1) + "] = ? ");
+                    mat[i, j] = Convert.ToInt32(Console.ReadLine()); ;
+                }
+            }
+
+            // Aplicar las secuencias a la Matriz
+            Console.Write("\n\n Prueba de Primera Secuencia\n");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(" " + mat[0, j]);
+            }
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i > 0) && (i < n - 1) && ((j == 0) || (j == n - 1)))
+                    {
+                        Console.Write(" " + mat[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+            }
+
+            Console.Write("\n");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(" " + mat[n - 1, j]);
+            }
+
+            Console.Write("\n\nPrueba de Segunda Secuencia\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i == j) || (i == (n - 1) - j))
+                    {
+                        Console.Write(" " + mat[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+            }
+
+            Console.Write("\n\nPrueba de Tercera Secuencia\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(" " + mat[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.Write("\n");
+            }
+
+            Console.Write("\n\nPrueba de Cuarta Secuencia\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        Console.Write(" " + mat[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+            }
+
+            // Aplicar las secuencias a la Matriz
+            Console.Write("\n\n Implementada Primera Secuencia\n");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(" 1");
+            }
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i > 0) && (i < n - 1) && ((j == 0) || (j == n - 1)))
+                    {
+                        Console.Write(" 1");
+                    }
+                    else
+                    {
+                        Console.Write(" 0");
+                    }
+                }
+            }
+
+            Console.Write("\n");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(" 1");
+            }
+
+            Console.Write("\n\n Implementada Segunda Secuencia\n");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i == j) || (i == (n - 1) - j))
+                    {
+                        Console.Write(" 1");
+                    }
+                    else
+                    {
+                        Console.Write(" 0");
+                    }
+                }
+            }
+
+            Console.Write("\n\n Implementada Tercera Secuencia\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(" 1");
+                    }
+                    else
+                    {
+                        Console.Write(" 0");
+                    }
+                }
+                Console.Write("\n");
+            }
+
+            Console.Write("\n\nImplementada Cuarta Secuencia\n");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < n; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        Console.Write(" 1");
+                    }
+                    else
+                    {
+                        Console.Write(" 0");
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\nHasta la proxima...\n");
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
---
+++ EJERCICIO_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int n;
+                Console.WriteLine("Ingrese el Vector N");
+                Console.Write("N: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+
+
+                int[] vec = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("\nvector[" + (i + 1) + "]=  ");
+                    vec[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("\n\nEl vector ingresado por el usuario es: \n");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(" " + vec[i]);
+                }
+
+                // Ordenar vector, metodo burbuja
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        if (vec[j] > vec[j + 1])
+                        {
+                            int aux = vec[j];
+                            vec[j] = vec[j + 1];
+                            vec[j + 1] = aux;
+                        }
+                    }
+                }
+
+                Console.WriteLine("\n\nEl elemento menor del vector es: " + vec[0]);
+                Console.WriteLine("\nEl elemento mayor del vector es: " + vec[n - 1]);
+                Console.WriteLine("\nEl vector ordenado es: \n");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(" " + vec[i]);
+                }
+
+                Console.WriteLine("Hasta la proxima.");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    
+
